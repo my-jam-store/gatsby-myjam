c3bc08d43718c18497fb0ed03121522c6ff2b48a
Ahmad Kayali <kayali1411@gmail.com>
2019-10-10 21:41:21 +0300

    fix data property name in createPage query

--- gatsby-node.js
+++ gatsby-node.js
@@ -13,7 +13,7 @@ exports.createPages = ({ graphql, actions }) => {
               name
               slug
               mainCategory
-              subCategory
+              subCategories
             }
           }
         }
