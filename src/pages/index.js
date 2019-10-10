8c56aaf920b0ccb2a7af1642b13140be825cf26f
Ahmad Kayali <kayali1411@gmail.com>
2019-10-10 21:31:26 +0300

    update query to load subCategories

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,7 @@ export default IndexPage
 
 export const categoriesQuery = graphql`
     {
-        allAirtable(filter: { table: { eq: "Categories" }, data: { mainCategory: { eq: 1 }}}) {
+        allAirtable(filter: { table: { eq: "Categories" }}) {
             nodes {
                 data {
                     categoryId
