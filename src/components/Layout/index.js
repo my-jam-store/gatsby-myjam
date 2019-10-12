4167791539aa7bea8ac745a475d26fbddeff5895
Ahmad Kayali <kayali1411@gmail.com>
2019-10-12 16:30:43 +0300

    wrap layout component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -7,22 +7,16 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import Header from "./Header"
+import Header from "../Header"
+import { Main } from "./Components"
 import "./layout.css"
 
-const Layout = ({ children, categories }) => {
+const Index = ({ children, categories }) => {
   return (
     <>
-      <Header categories={categories} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
+      <Header categories={categories || []} />
+      <div>
+        <Main>{children}</Main>
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
@@ -33,9 +27,9 @@ const Layout = ({ children, categories }) => {
   )
 }
 
-Layout.propTypes = {
+Index.propTypes = {
   children: PropTypes.node.isRequired,
   categories: PropTypes.array
 }
 
-export default Layout
+export default Index
