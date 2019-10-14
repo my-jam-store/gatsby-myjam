77b6b2783704e2ef8cf8346039c7a3993a734311
Ahmad Kayali <kayali1411@gmail.com>
2019-10-14 22:32:28 +0300

    update category page template

--- src/templates/category.js
+++ src/templates/category.js
@@ -1,32 +1,31 @@
 import React from "react"
 import { graphql } from "gatsby"
-import Index from "../components/Layout"
+import Layout from "../components/Layout"
 import SEO from "../components/seo"
-import ProductGrid from "../components/ProductGrid"
+import View from "../components/Category"
+import { GlobalStateContext } from "../store/globalState.js"
 
-const categoryPage = ({ data }) => {
-  const { category, products, allCategories } = data
+const categoryPage = ({ data, pageContext }) => {
+  const { allCategories } = data
 
   return (
-    <Index categories={allCategories.nodes}>
-      <SEO title={category.data.name} />
-      <div>
-        <ProductGrid title={category.data.name} products={products.nodes} />
-      </div>
-    </Index>
+    <Layout categories={allCategories.nodes}>
+      <GlobalStateContext.Consumer>
+        {globalState => (
+          <>
+            <SEO title={pageContext.name} />
+            <View globalState={globalState} pageContext={pageContext} />
+          </>
+        )}
+      </GlobalStateContext.Consumer>
+    </Layout>
   )
 }
 
 export default categoryPage
 
 export const pageQuery = graphql`
-    query($id: Int!, $recordId: String!) {
-        category: airtable(data: { categoryId : { eq: $id } }){
-            data {
-                categoryId
-                name
-            }
-        }
+    query{
         allCategories: allAirtable(filter: { table: { eq: "Categories" }}) {
             nodes {
                 recordId
@@ -36,18 +35,6 @@ export const pageQuery = graphql`
                     subCategories
                     name
                     slug
-                }
-            }
-        }
-        products: allAirtable(filter: {table: { eq: "Products" }, data: {categories: { eq: $recordId}}}) {
-            nodes {
-                data {
-                    productId
-                    name
-                    slug
-                    sku
-                    price
-                    categories
                 }
             }
         }
