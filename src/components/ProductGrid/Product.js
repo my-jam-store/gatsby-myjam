a340d8a2ab15f3513ae1eb7ba28e05038e5e5b8d
Ahmad Kayali <kayali1411@gmail.com>
2019-10-15 18:16:11 +0300

    updat product grid to handle infinite scroll

--- src/components/ProductGrid/Product.js
+++ src/components/ProductGrid/Product.js
@@ -1,14 +1,23 @@
 import React from "react"
 import { Item } from "./Components"
 
-const Product = ({ data }) => {
-  const { productId, name, price, sku } = data
+const Product = ({ js, item }) => {
+  if(js) {
+    console.log(item.data)
+  }
   return (
-    <Item key={productId}>
-      <img src={`https://res.cloudinary.com/${process.env.GATSBY_CLOUDINARY_KEY}/image/upload/${process.env.GATSBY_CLOUDINARY_PATH}/my-jam/${sku}.jpg`} alt={name} />
-      <h3>{name}</h3>
-      <span>{price}</span>
-    </Item>
+    <>
+      {js && (
+        <Item key={item.data.productId}>
+            <img
+              src={`https://res.cloudinary.com/${process.env.GATSBY_CLOUDINARY_KEY}/image/upload/${process.env.GATSBY_CLOUDINARY_PATH}/my-jam/${item.data.sku}.jpg`}
+              alt={item.data.name}
+            />
+            <h3>{item.data.name}</h3>
+            <span>{item.data.price}</span>
+        </Item>
+      )}
+    </>
   )
 }
 
--- src/components/ProductGrid/index.js
+++ src/components/ProductGrid/index.js
@@ -1,16 +1,65 @@
 import React from "react"
 import { Grid, Title } from "./Components"
-import Product from "./Product"
+import GridItem from "./Product"
 
-const ProductGrid = ({ products, title }) => {
+const ProductGrid = (props) => {
+  const g = props.globalState
+  const currentPage = props.pageContext.currentPage
+
+  let i = 0, j = 0 , numberOfItemsOnLatestPage = 0
+
+  const js = !g.isInitializing()
+  const items = []
+
+
+  if(g.useInfiniteScroll && g["page"+currentPage]) {
+    for(let pageNum = currentPage ;; pageNum++) {
+      const key = "page"+pageNum
+      if (g[key]) {
+        numberOfItemsOnLatestPage = g[key].length
+        for(j = 0; j < numberOfItemsOnLatestPage; j++) {
+          items.push(<GridItem js={js} item={g[key][j]} key={"gi"+(i++)} />)
+        }
+      }
+      else {
+        const lastFetchedPage = Math.min(g.cursor, props.pageContext.countPages)
+        for (; pageNum <= lastFetchedPage; pageNum++) {
+          for (j = 0; j < numberOfItemsOnLatestPage; j++) {
+            items.push(<GridItem key={"gi"+(i++)}/>)
+          }
+        }
+        break;
+      }
+    }
+  } else {
+    props.pageContext.pageProducts.forEach(item => items.push(<GridItem item={item} key={"gi"+(i++)}/>))
+  }
+
   return (
     <>
-      <Title>{title}</Title>
+      <Title>{props.pageContext.name}</Title>
       <Grid>
-        {products.map(({ data }) => ( <Product key={data.productId} data={data} /> ))}
+        {items}
       </Grid>
     </>
   )
 }
 
-export default ProductGrid
+export default ProductGrid;
+
+// import React from "react"
+// import { Grid, Title } from "./Components"
+// import Product from "./Product"
+//
+// const ProductGrid = ({ products, title }) => {
+//   return (
+//     <>
+//       <Title>{title}</Title>
+//       <Grid>
+//         {products.map(({ data }) => ( <Product key={data.productId} data={data} /> ))}
+//       </Grid>
+//     </>
+//   )
+// }
+//
+// export default ProductGrid
