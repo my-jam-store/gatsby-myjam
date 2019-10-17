620165a181a53b242a932f93d6150231382e4f5b
Ahmad Kayali <kayali1411@gmail.com>
2019-10-17 17:46:33 +0300

    add snipcart test account

--- src/components/seo.js
+++ src/components/seo.js
@@ -68,7 +68,17 @@ function SEO({ description, lang, meta, title }) {
           content: metaDescription,
         },
       ].concat(meta)}
-    />
+    >
+      <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+              integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+              crossOrigin="anonymous">
+      </script>
+      <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js"
+              id="snipcart"
+              data-api-key="NjMyNDlmZTEtMTEzMS00YjU2LWI0YjQtZGU2NjBmMGM0YjY1NjM3MDU3ODM1NzExNDE0MDk4">
+      </script>
+      <link href="https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css" type="text/css" rel="stylesheet"/>
+    </Helmet>
   )
 }
 
