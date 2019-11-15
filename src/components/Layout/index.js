5c6e78e9073b312c167ee888ff3f4564c0117faf
Ahmad Kayali <kayali1411@gmail.com>
2019-11-15 17:05:59 +0200

    add videoask embed code

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -5,7 +5,7 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
+import React, { useEffect } from "react"
 import PropTypes from "prop-types"
 import ReactNotification from "react-notifications-component"
 import Header from "../Header"
@@ -13,6 +13,30 @@ import { Main } from "./Components"
 import "./layout.css"
 
 const Index = ({ children, categories }) => {
+  useEffect(() => {
+    if(typeof window !== "undefined") {
+      window.VIDEOASK_EMBED_CONFIG = {
+        "kind": "widget",
+        "url": "https://www.videoask.com/f71gdblja",
+        "options": {
+          "widgetType": "VideoThumbnailExtraLarge",
+          "text": "Talk to me",
+          "backgroundColor": "#440E5E",
+          "position": "bottom-left"
+        }
+      }
+
+      const script = document.createElement('script')
+      script.id   = 'videoask'
+      script.src   = 'https://www.videoask.com/embed/embed.js'
+      script.type  = 'text/javascript'
+
+      if(!document.getElementById('videoask')) {
+        document.body.appendChild(script)
+      }
+    }
+  }, [])
+
   return (
     <>
       <ReactNotification />
