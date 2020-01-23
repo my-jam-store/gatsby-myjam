5e00dcfbc8f0d5a5bd0ef8b7b4b0b45ce38c99e4
Moe Ghashim <mohanadgh@gmail.com>
2020-01-23 11:42:11 +0000

    added google analytics meta

--- src/html.js
+++ src/html.js
@@ -13,6 +13,10 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
+        <meta
+          name="google-site-verification"
+          content="kBG4QRnwAhl2l-Lobh_6uc0K7aeJhKELgXWa_OTgOi0"
+        />
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
