29abd043a8bfad9c6b80e7b54c5937d461369dc8
Ahmad Kayali <kayali1411@gmail.com>
2020-01-24 22:17:49 +0200

    working on infinite sroll

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -4,33 +4,10 @@ import ProductGrid from "../ProductGrid"
 import AppContext from "../../store/context"
 
 const View = (props) => {
-  const [ cursor, setCursor ] = useState(0)
-  const [ pages, setPages ]   = useState({})
+
   const [ store, setStore ]   = useState('')
   const { state, dispatch }   = useContext(AppContext)
   const { pageContext } = props
-
-  const isInitializing = () => cursor === 0
-
-  const loadMore = (pageContext, pageNum) => {
-    setCursor(pageNum + 1)
-    fetch(`${__PATH_PREFIX__}/paginationJson/${pageContext.type}-${pageContext.slug}${pageNum}.json`)
-      .then(res => res.json())
-      .then(
-        res => {
-          setPages({
-            ...pages,
-            ["page"+pageNum]: res
-          })
-        },
-        error => console.log(error)
-      )
-  }
-
-  const hasMore = (countPages) => {
-    if (isInitializing()) return true
-    return cursor <= countPages
-  }
 
   useEffect(() => {
     if(pageContext.type === 'store') {
@@ -41,16 +18,6 @@ const View = (props) => {
       })
       setStore(pageContext.name)
     }
-
-    const pageKey = "page" + pageContext.currentPage
-
-    setPages({
-      ...pages,
-      [pageKey]: pageContext.pageProducts
-    })
-    setTimeout(() => {
-      setCursor(pageContext.currentPage + 1)
-    }, 1000)
   }, [])
 
   useEffect(() => {
@@ -60,30 +27,7 @@ const View = (props) => {
   }, [store])
 
   return (
-    <>
-      <InfiniteScroll
-        cursor={cursor}
-        throttle={300}
-        threshold={600}
-        hasMore={hasMore(pageContext.countPages)}
-        pageContext={pageContext}
-        onLoadMore={loadMore}
-      >
-        <ProductGrid
-          pageContext={pageContext}
-          globalState={{
-            cursor,
-            ...pages,
-          }}
-        />
-      </InfiniteScroll>
-
-      {(cursor === 0 || hasMore(pageContext.countPages)) && (
-        <h3>
-          Loading.....
-        </h3>
-      )}
-    </>
+    <ProductGrid pagesCounts={pageContext.countPages} title={pageContext.name} />
   )
 }
 
--- src/components/ProductGrid/index.js
+++ src/components/ProductGrid/index.js
@@ -2,43 +2,17 @@ import React, { useEffect, useState, useRef } from "react"
 import { Grid, Title } from "./Components"
 import GridItem from "./Product"
 import ProductModal from "./ProductModal"
+import InfiniteScroll from "react-infinite-scroll-component"
 
-const ProductGrid = (props) => {
-  const [ item, setItem ] = useState({})
+const ProductGrid = ({ pagesCounts, title }) => {
+  const [ item, setItem ]   = useState({})
+  const [ items, setItems ] = useState([])
+  const [ hasMore, setHasMore ] = useState(true)
   const [ showModal, setModalState ] = useState(false)
 
   const componentMounted = useRef(false)
 
-  const g = props.globalState
-  const currentPage = props.pageContext.currentPage
-
-  let i = 0, j = 0 , numberOfItemsOnLatestPage = 0
-
-  const js = true
-  const items = []
-
   const isMobile = () => typeof window !== "undefined" && window.innerWidth <= 768
-
-  if(g["page"+currentPage]) {
-    for(let pageNum = currentPage ;; pageNum++) {
-      const key = "page"+pageNum
-      if (g[key]) {
-        numberOfItemsOnLatestPage = g[key].length
-        for(j = 0; j < numberOfItemsOnLatestPage; j++) {
-          items.push(<GridItem js={js} item={g[key][j]} key={"gi"+(i++)} />)
-        }
-      }
-      else {
-        const lastFetchedPage = Math.min(g.cursor, props.pageContext.countPages)
-        for (; pageNum <= lastFetchedPage; pageNum++) {
-          for (j = 0; j < numberOfItemsOnLatestPage; j++) {
-            items.push(<GridItem key={"gi"+(i++)}/>)
-          }
-        }
-        break;
-      }
-    }
-  }
 
   const handleProductModal = (e) => {
     if(!isMobile() && e.target.closest('div').getAttribute('data-id')) {
@@ -58,6 +32,15 @@ const ProductGrid = (props) => {
 
   const handleCloseModal = () => setModalState(false)
 
+  const fetchMoreData = () => {
+    if(items.length >= pagesCounts) {
+      setHasMore(false)
+      return;
+    }
+
+    //TODO fetch items....
+  };
+
   useEffect(() => {
     if(componentMounted.current) {
       setModalState(true)
@@ -66,9 +49,16 @@ const ProductGrid = (props) => {
 
   return (
     <>
-      <Title>{props.pageContext.name}</Title>
+      <Title>{title}</Title>
       <Grid onClick={handleProductModal}>
-        {items}
+        <InfiniteScroll
+          dataLength={items.length}
+          next={fetchMoreData}
+          hasMore={hasMore}
+          loader={<h4>Loading...</h4>}
+        >
+          {items}
+        </InfiniteScroll>
       </Grid>
       <ProductModal
         isOpen={showModal}
