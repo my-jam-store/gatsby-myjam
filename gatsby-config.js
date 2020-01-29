d7bdef3880d58ce3b3669174fa609da56cf2c612
Ahmad Kayali <kayali1411@gmail.com>
2020-01-29 18:59:27 +0200

    working on PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,17 +77,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `My Jam`,
+        icon: `src/images/logo.png`,
+        short_name: `My Jam`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+        background_color: `#FFF`,
+        theme_color: `#000`,
+        display: `standalone`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`
   ],
 }
