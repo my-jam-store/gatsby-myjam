71938acae1d75be314ac39faae888c813feae2aa
Ahmad Kayali <kayali1411@gmail.com>
2020-03-20 14:43:38 +0200

    add videoAsk to homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,7 +6,17 @@ export default (props) => {
   const cuisines = props.cuisines.filter(cuisine => cuisine.data.slug !== 'default')
   return (
     <Grid>
-      {cuisines.map(cuisine => (<Cuisine slug={cuisine.data.slug} name={cuisine.data.name} key={cuisine.recordId} />))}
+      <iframe src="https://www.videoask.com/f3ytzr0u3"
+              allow="camera; microphone; autoplay; encrypted-media;"
+              width="100%"
+              height="600px"
+              style={{
+                border: 'none',
+                borderRadius: '24px'
+              }}
+      >
+      </iframe>
+      {/*{cuisines.map(cuisine => (<Cuisine slug={cuisine.data.slug} name={cuisine.data.name} key={cuisine.recordId} />))}*/}
     </Grid>
   )
 }
