bc2019c478eb028883bbe2c9061925190a8ba4bd
Ahmad Kayali <kayali1411@gmail.com>
2020-03-20 15:15:28 +0200

    working on videoAsk design

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -9,17 +9,19 @@ export default (props) => {
       <iframe src='https://www.videoask.com/f3ytzr0u3'
               allow='camera; microphone; autoplay; encrypted-media;'
               width='100%'
-              height='400px'
+              height='650px'
               style={{
                 border: 'none',
-                borderRadius: '24px',
+                borderRadius: '10px',
                 gridColumn: '1/-1',
                 boxShadow: '0 1px 6px 0 rgba(133,123,123,.75)',
                 marginBottom: '0px'
               }}
       >
       </iframe>
-      {cuisines.map(cuisine => (<Cuisine slug={cuisine.data.slug} name={cuisine.data.name} key={cuisine.recordId} />))}
+      {cuisines.map(cuisine => (
+          <Cuisine slug={cuisine.data.slug} name={cuisine.data.name} key={cuisine.recordId} />
+        ))}
     </Grid>
   )
 }
