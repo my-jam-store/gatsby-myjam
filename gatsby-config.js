e4043382a19e4a261c1be8729eada139e0e8f4c7
Ahmad Kayali <kayali1411@gmail.com>
2020-03-27 21:29:02 +0300

    update data format

--- gatsby-config.js
+++ gatsby-config.js
@@ -120,12 +120,13 @@ module.exports = {
             serialize: ({ query: { allAirtable } }) => {
               return allAirtable.nodes.slice(100,102).map(node => {
                 return {
-                  "ID": `sku_id_ve2_${node.recordId}`,
-                  "item_group_id": `prod_id_ve2_${node.recordId}`,
+                  "ID": `sku_id_ve3_${node.recordId}`,
+                  "item_group_id": `prod_id_ve3_${node.recordId}`,
                   // "item_group_id": "prod_id_849031",
                   "title": node.data.name,
                   "name": node.data.name,
-                  "inventory": "infinite",
+                  "inventory.type": "infinite",
+                  "inventory_type": "infinite",
                   // "description": `Description`,
                   // "link": `https://zen-colden-f5dbb1.netlify.com/products`,
                   "image_link": `https://res.cloudinary.com/${process.env.GATSBY_CLOUDINARY_KEY}/image/upload/${process.env.GATSBY_CLOUDINARY_PATH}/my-jam/${node.data.sku}.jpg`,
