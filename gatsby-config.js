063b4fbd8d763768739384c65b7824015f0985d4
Ahmad Kayali <kayali1411@gmail.com>
2020-03-28 07:39:03 +0300

    update data format

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,14 +118,15 @@ module.exports = {
               }
             `,
             serialize: ({ query: { allAirtable } }) => {
-              return allAirtable.nodes.slice(100,102).map(node => {
+              return allAirtable.nodes.slice(100,101).map(node => {
                 return {
-                  // "ID": `sku_id_v_${node.recordId}`,
+                  "ID": `sku_id_v_${node.recordId}`,
                   "item_group_id": `prod_id_v_${node.recordId}`,
                   // "item_group_id": "prod_id_849031",
                   "title": node.data.name,
                   "name": node.data.name,
                   "size": "Small",
+                  "attributes": "name,size",
                   // "description": `Description`,
                   // "link": `https://zen-colden-f5dbb1.netlify.com/products`,
                   "image_link": `https://res.cloudinary.com/${process.env.GATSBY_CLOUDINARY_KEY}/image/upload/${process.env.GATSBY_CLOUDINARY_PATH}/my-jam/${node.data.sku}.jpg`,
