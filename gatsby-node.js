b0db18561ef51700dd546b2e6a9d34580d7bb651
Ahmad Kayali <kayali1411@gmail.com>
2020-04-03 08:03:17 +0300

    remove cuisines and categories queries

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,33 +5,7 @@ exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
   return graphql(
     `
-      {
-        categories: allAirtable(filter: {table: {eq: "Categories"}}) {
-          totalCount
-          nodes {
-            recordId
-            data {
-              categoryId
-              name
-              slug
-              mainCategory
-              subCategories
-            }
-          }
-        }
-        
-        cuisines: allAirtable(filter: {table: {eq: "Cuisines"}}) {
-          totalCount
-          nodes {
-            recordId
-            data {
-              cuisineId
-              name
-              slug
-            }
-          }
-        }
-        
+      { 
         stores: allAirtable(filter: {table: {eq: "Stores"}}) {
           totalCount
           nodes {
@@ -54,8 +28,6 @@ exports.createPages = ({ graphql, actions }) => {
               sku
               description
               price
-              categories
-              cuisines
               stores
               simply_jazz_mental
               valve_brass_statue
@@ -79,7 +51,7 @@ exports.createPages = ({ graphql, actions }) => {
       console.log(`Error retrieving categories data`, result.errors)
     }
 
-    const { categories, products, cuisines, stores, pages } = result.data
+    const { products, stores, pages } = result.data
 
     pages.edges.forEach(({ node }) => {
       createPage({
@@ -89,19 +61,18 @@ exports.createPages = ({ graphql, actions }) => {
       })
     })
 
-    const categoryTemplate = path.resolve(`./src/templates/category.js`)
-    const cuisineTemplate  = path.resolve(`./src/templates/cuisine.js`)
-    const storeTemplate    = path.resolve(`./src/templates/store.js`)
+    const storeTemplate = path.resolve(`./src/templates/store.js`)
     const countProductsPerPage = 30
 
     for(let i = 0; i < stores.totalCount; i++) {
       const totalProductsPerStore = products.nodes.filter((product) => (
-        product.data.stores.indexOf(stores.nodes[i].recordId)) !== -1
-      )
+        product.data.stores.indexOf(stores.nodes[i].recordId) !== -1
+      ))
 
       if(totalProductsPerStore.length === 0) continue
 
       const countPages = Math.ceil(totalProductsPerStore.length / countProductsPerPage)
+      const storeCode = stores.nodes[i].data.storeCode.substr(3).replace(/\./g,'-');
 
       for(let currentPage = 1; currentPage <= countPages; currentPage++) {
         const pathSuffix = currentPage > 1 ? currentPage : ""
@@ -109,17 +80,16 @@ exports.createPages = ({ graphql, actions }) => {
         const startIndexInclusive = countProductsPerPage * (currentPage - 1)
         const endIndexExclusive = startIndexInclusive + countProductsPerPage
         const pageProducts = totalProductsPerStore.slice(startIndexInclusive, endIndexExclusive)
-
         const pageData = {
           filePath: `/${pathSuffix}`,
-          path: `/store/${stores.nodes[i].data.storeCode.substr(3).replace(/\./g,'-')}/${pathSuffix}`,
+          path: `/store/${storeCode}/${pathSuffix}`,
           component: storeTemplate,
           context: {
             name: stores.nodes[i].data.name,
             pageProducts: pageProducts,
             currentPage: currentPage,
             countPages: countPages,
-            slug: stores.nodes[i].data.storeCode.substr(3).replace(/\./g,'-'),
+            slug: storeCode,
             recordId: stores.nodes[i].recordId,
             id: stores.nodes[i].recordId,
             type: 'store'
@@ -131,7 +101,7 @@ exports.createPages = ({ graphql, actions }) => {
       }
 
       const storePageData = {
-        path: `/store/${stores.nodes[i].data.storeCode.substr(3).replace(/\./g,'_')}/products`,
+        path: `/store/${storeCode}/products`,
         component: path.resolve(`./src/templates/storeProducts.js`),
         context: {
           name: stores.nodes[i].data.name,
@@ -142,81 +112,6 @@ exports.createPages = ({ graphql, actions }) => {
         }
       }
       createPage(storePageData)
-    }
-
-    for(let i = 0; i < cuisines.totalCount; i++) {
-      if(cuisines.nodes[i].slug === 'default') continue
-
-      const totalProductsPerCuisine = products.nodes.filter((product) => (
-        product.data.cuisines.indexOf(cuisines.nodes[i].recordId)) !== -1
-      )
-
-      if(totalProductsPerCuisine.length === 0) continue
-
-      const countPages = Math.ceil(totalProductsPerCuisine.length / countProductsPerPage)
-
-      for(let currentPage = 1; currentPage <= countPages; currentPage++) {
-        const pathSuffix = currentPage > 1 ? currentPage : ""
-
-        const startIndexInclusive = countProductsPerPage * (currentPage - 1)
-        const endIndexExclusive = startIndexInclusive + countProductsPerPage
-        const pageProducts = totalProductsPerCuisine.slice(startIndexInclusive, endIndexExclusive)
-
-        const pageData = {
-          filePath: `/${pathSuffix}`,
-          path: `/cuisine/${cuisines.nodes[i].data.slug}/${pathSuffix}`,
-          component: cuisineTemplate,
-          context: {
-            name: cuisines.nodes[i].data.name,
-            pageProducts: pageProducts,
-            currentPage: currentPage,
-            countPages: countPages,
-            slug: cuisines.nodes[i].data.slug,
-            recordId: cuisines.nodes[i].recordId,
-            id: cuisines.nodes[i].recordId,
-            type: 'cuisine'
-          }
-        }
-
-        createJSON(pageData)
-        createPage(pageData)
-      }
-    }
-
-    for(let i = 0; i < categories.totalCount; i++) {
-      const totalProductsPerCategory = products.nodes.filter((product) => (
-        product.data.categories.indexOf(categories.nodes[i].recordId)) !== -1
-      )
-
-      const countPages = Math.ceil(totalProductsPerCategory.length / countProductsPerPage)
-
-      for(let currentPage = 1; currentPage <= countPages; currentPage++) {
-        const pathSuffix = currentPage > 1 ? currentPage : ""
-
-        const startIndexInclusive = countProductsPerPage * (currentPage - 1)
-        const endIndexExclusive = startIndexInclusive + countProductsPerPage
-        const pageProducts = totalProductsPerCategory.slice(startIndexInclusive, endIndexExclusive)
-
-        const pageData = {
-          filePath: `/${pathSuffix}`,
-          path: `/category/${categories.nodes[i].data.slug}/${pathSuffix}`,
-          component: categoryTemplate,
-          context: {
-            name: categories.nodes[i].data.name,
-            pageProducts: pageProducts,
-            currentPage: currentPage,
-            countPages: countPages,
-            slug: categories.nodes[i].data.slug,
-            recordId: categories.nodes[i].recordId,
-            id: categories.nodes[i].data.categoryId,
-            type: 'category'
-          }
-        }
-
-        createJSON(pageData)
-        createPage(pageData)
-      }
-      console.log(`\nCreated ${countPages} pages of paginated content.`)
     }
   })
     .catch(error => {
