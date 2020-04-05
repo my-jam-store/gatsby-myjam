b9046653b31de1c9f26adda2d1d3ed643ef37a09
Ahmad Kayali <kayali1411@gmail.com>
2020-04-05 09:37:01 +0300

    remove videoAsk

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,19 +6,6 @@ export default (props) => {
   const cuisines = props.cuisines.filter(cuisine => cuisine.data.slug !== 'default')
   return (
     <Grid>
-      <iframe src='https://www.videoask.com/f3ytzr0u3'
-              allow='camera; microphone; autoplay; encrypted-media;'
-              width='100%'
-              height='650px'
-              style={{
-                border: 'none',
-                borderRadius: '10px',
-                gridColumn: '1/-1',
-                boxShadow: '0 1px 6px 0 rgba(133,123,123,.75)',
-                marginBottom: '0px'
-              }}
-      >
-      </iframe>
       {cuisines.map(cuisine => (
           <Cuisine slug={cuisine.data.slug} name={cuisine.data.name} key={cuisine.recordId} />
         ))}
