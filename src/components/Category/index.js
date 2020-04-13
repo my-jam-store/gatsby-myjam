971a73f099a690dcbacfbfa3651d95e6997d1339
Ahmad Kayali <kayali1411@gmail.com>
2020-04-13 16:23:52 +0300

    remove unused code

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -4,23 +4,14 @@ import AppContext from "../../store/context"
 import { setStoreAction } from "../../store/actions"
 
 const View = (props) => {
-
-  const [ store, setStore ] = useState('')
-  const { state, dispatch } = useContext(AppContext)
+  const { dispatch } = useContext(AppContext)
   const { pageContext } = props
 
   useEffect(() => {
     if(pageContext.type === 'store') {
       dispatch(setStoreAction(pageContext.name, pageContext.slug.replace(/-/g,'_')))
-      setStore(pageContext.name)
     }
   }, [])
-
-  useEffect(() => {
-    if(!!store) {
-      localStorage.setItem('globalStore', JSON.stringify(state))
-    }
-  }, [store])
 
   return (
     <ProductGrid
