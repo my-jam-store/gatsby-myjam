1167df0beb9fcf41ef6f3a25a38949481710fe86
Ahmad Kayali <kayali1411@gmail.com>
2020-04-26 00:44:52 +0300

    handle window undefined for build

--- src/components/MiniCart/EmptyCart.js
+++ src/components/MiniCart/EmptyCart.js
@@ -7,11 +7,13 @@ const EmptyCart = () => {
   const { state, dispatch } = useContext(AppContext)
 
   useEffect(() => {
-    const sessionId = window.location.search.replace('?session_id=', '');
-    if(state.sessionId === sessionId) {
-      dispatch(clearCart())
-    } else {
-      navigate("/404")
+    if (typeof window !== "undefined") {
+      const sessionId = window.location.search.replace('?session_id=', '');
+      if(state.sessionId === sessionId) {
+        dispatch(clearCart())
+      } else {
+        navigate("/404")
+      }
     }
   }, [])
 
