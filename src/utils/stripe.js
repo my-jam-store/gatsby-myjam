acfcf8e4f55c65fadace04dfa6ab795abe60dd30
Ahmad Kayali <kayali1411@gmail.com>
2020-04-27 22:12:00 +0300

    change the env variable name

--- src/utils/stripe.js
+++ src/utils/stripe.js
@@ -1,20 +1,19 @@
 import { loadStripe } from '@stripe/stripe-js';
 
 export const getStripeInstance = async () => {
-  console.log(process.env.STRIPE_PUBLIC_KEY);
-  const stripe = await loadStripe(process.env.STRIPE_PUBLIC_KEY)
+  console.log(process.env.GATSBY_STRIPE_PUBLIC_KEY, process.env.GATSBY_CREATE_CHECKOUT_SESSION_API);
+  const stripe = await loadStripe(process.env.GATSBY_STRIPE_PUBLIC_KEY)
   return stripe
 }
 
 export const generateCheckoutSession = async (items) => {
-  const url = process.env.CREATE_CHECKOUT_SESSION_API
+  const url = process.env.GATSBY_CREATE_CHECKOUT_SESSION_API
   const options = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(items)
   }
 
-  console.log(JSON.stringify(items));
   const response = await fetch(url, options)
   const json = await response.json()
   return json
